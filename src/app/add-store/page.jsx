e66180a2ea67552ae910e1f378edaf67869cf90d
Ökkeş Akkurt <okkesakkurt46@gmail.com>
2023-12-04 11:35:19 +0300

    first commit

---
+++ src/app/add-store/page.jsx
@@ -0,0 +1,287 @@
+'use client'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import Link from 'next/link';
+import { useState } from 'react';
+
+const AddStore = () => {
+
+	const storedStores = JSON.parse(localStorage.getItem('stores')) || [];
+
+	const [ stores, setStores ] = useState(storedStores);
+
+
+	const handleOutsideSubmit = (values, e) => {
+		e.preventDefault();
+
+		const newId = stores.length;
+
+		const valuesWithId = {
+			...values,
+			id: newId,
+		};
+
+		const isStoreExist = stores.some((store) => store.storeName === values.storeName);
+
+		if (isStoreExist) {
+			console.log('Store already exists!');
+			alert('Bu mağaza zaten var!');
+		} else {
+			setStores((prevStores) => {
+				const updatedStores = [...prevStores, valuesWithId];
+				localStorage.setItem('stores', JSON.stringify(updatedStores));
+				return updatedStores;
+			});
+			console.log('One store added:', valuesWithId);
+			alert('Bir mağaza eklendi.');
+			formik.resetForm();
+		}
+	};
+
+
+	const formik = useFormik({
+		initialValues: {
+			id: '',
+			storeName: '',
+			country: '',
+			state: '',
+			address: '',
+			phone: '',
+			discountRate: '',
+			primeRate: '',
+			description: '',
+		},
+		validationSchema: Yup.object({
+			storeName: Yup.string().required('Store name is required'),
+			country: Yup.string().required('Country is required'),
+			state: Yup.string().required('State is required'),
+			address: Yup.string().required('Address is required'),
+			phone: Yup.string().required('Phone number is required'),
+			discountRate: Yup.number().required('Discount rate is required').positive('Discount rate must be positive'),
+			primeRate: Yup.number().required('Prime rate is required').positive('Prime rate must be positive'),
+			description: Yup.string(),
+		}),
+		onSubmit: () => {},
+		validateOnBlur: false,
+		validateOnChange: false,
+	});
+
+	const stateOptions = [...new Set([...stores.map((store) => store.state), formik.values.state])].filter(Boolean);
+
+
+	const handleStateChange = (selectedState) => {
+		formik.setFieldValue('state', selectedState);
+	};
+
+	return (
+		<div className='max-w-lg mt-6 bg-slate-400 p-4 md:p-8 mx-auto'>
+			<div>
+				<form>
+					<div className='mb-4'>
+						<label
+							htmlFor='storeName'
+							className='block text-sm font-medium text-gray-600'>
+							Mağaza İsmi:
+						</label>
+						<input
+							type='text'
+							id='storeName'
+							name='storeName'
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							value={formik.values.storeName}
+							className='mt-1 p-2 border rounded-md w-full'
+						/>
+						{formik.touched.storeName && formik.errors.storeName && (
+							<div className='text-red-500 mt-1 text-sm'>{formik.errors.storeName}</div>
+						)}
+					</div>
+
+					<div className='mb-4'>
+						<label
+							htmlFor='country'
+							className='block text-sm font-medium text-gray-600'>
+							Ülke:
+						</label>
+						<input
+							type='text'
+							id='country'
+							name='country'
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							value={formik.values.country}
+							className='mt-1 p-2 border rounded-md w-full'
+						/>
+						{formik.touched.country && formik.errors.country && (
+							<div className='text-red-500 mt-1 text-sm'>{formik.errors.country}</div>
+						)}
+					</div>
+
+					<div className='mb-4'>
+						<label
+							htmlFor='state'
+							className='block text-sm font-medium text-gray-600'>
+							Eyalet:
+						</label>
+						<div className='flex flex-col md:flex-row'>
+							<select
+								id='state'
+								name='state'
+								onChange={(e) => {
+									formik.handleChange(e);
+									handleStateChange(e.target.value);
+								}}
+								onBlur={formik.handleBlur}
+								value={formik.values.state}
+								className='mt-1 p-2 border rounded-md mb-2 md:mb-0 md:w-1/2'>
+								<option
+									value=''
+									disabled>
+									Select a state
+								</option>
+								{stateOptions.map((state) => (
+									<option
+										key={state}
+										value={state}>
+										{state}
+									</option>
+								))}
+							</select>
+							<input
+								type='text'
+								id='stateInput'
+								name='stateInput'
+								onChange={(e) => {
+									formik.handleChange(e);
+									handleStateChange(e.target.value);
+								}}
+								onBlur={formik.handleBlur}
+								value={formik.values.state}
+								placeholder='Enter a new state'
+								className='mt-1 p-2 border rounded-md ml-0 md:ml-2 md:w-1/2'
+							/>
+						</div>
+						{formik.touched.state && formik.errors.state && (
+							<div className='text-red-500 mt-1 text-sm'>{formik.errors.state}</div>
+						)}
+					</div>
+
+					<div className='mb-4'>
+						<label
+							htmlFor='address'
+							className='block text-sm font-medium text-gray-600'>
+							Açık Adres:
+						</label>
+						<input
+							type='text'
+							id='address'
+							name='address'
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							value={formik.values.address}
+							className='mt-1 p-2 border rounded-md w-full'
+						/>
+						{formik.touched.address && formik.errors.address && (
+							<div className='text-red-500 mt-1 text-sm'>{formik.errors.address}</div>
+						)}
+					</div>
+
+					<div className='mb-4'>
+						<label
+							htmlFor='phone'
+							className='block text-sm font-medium text-gray-600'>
+							Telefon:
+						</label>
+						<input
+							type='text'
+							id='phone'
+							name='phone'
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							value={formik.values.phone}
+							className='mt-1 p-2 border rounded-md w-full'
+						/>
+						{formik.touched.phone && formik.errors.phone && (
+							<div className='text-red-500 mt-1 text-sm'>{formik.errors.phone}</div>
+						)}
+					</div>
+
+					<div className='mb-4'>
+						<label
+							htmlFor='discountRate'
+							className='block text-sm font-medium text-gray-600'>
+							İndirim Oranı Maksimum(%):
+						</label>
+						<input
+							type='text'
+							id='discountRate'
+							name='discountRate'
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							value={formik.values.discountRate}
+							className='mt-1 p-2 border rounded-md w-full'
+						/>
+						{formik.touched.discountRate && formik.errors.discountRate && (
+							<div className='text-red-500 mt-1 text-sm'>{formik.errors.discountRate}</div>
+						)}
+					</div>
+
+					<div className='mb-4'>
+						<label
+							htmlFor='primeRate'
+							className='block text-sm font-medium text-gray-600'>
+							Prim Oranı Maksimum(%):
+						</label>
+						<input
+							type='text'
+							id='primeRate'
+							name='primeRate'
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							value={formik.values.primeRate}
+							className='mt-1 p-2 border rounded-md w-full'
+						/>
+						{formik.touched.primeRate && formik.errors.primeRate && (
+							<div className='text-red-500 mt-1 text-sm'>{formik.errors.primeRate}</div>
+						)}
+					</div>
+
+					<div className='mb-4'>
+						<label
+							htmlFor='description'
+							className='block text-sm font-medium text-gray-600'>
+							Açıklama:
+						</label>
+						<textarea
+							id='description'
+							name='description'
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							value={formik.values.description}
+							className='mt-1 p-2 border rounded-md w-full'
+						/>
+						{formik.touched.description && formik.errors.description && (
+							<div className='text-red-500 mt-1 text-sm'>{formik.errors.description}</div>
+						)}
+					</div>
+
+					<button
+						type='submit'
+						onClick={(e) => handleOutsideSubmit(formik.values, e)}
+						className='w-full md:w-auto px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600'>
+						Mağaza Ekle
+					</button>
+				</form>
+
+				<p className='mt-4 text-center md:text-left'>
+					<Link href='/'>
+						<button className='text-blue-500 hover:underline'>Mağaza Listesi</button>
+					</Link>
+				</p>
+			</div>
+		</div>
+	);
+
+};
+
+export default AddStore;
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,358 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+const Home = () => {
+	let storedData = JSON.parse(localStorage.getItem('stores')) || [];
+	const [stores, setStores] = useState(storedData);
+	const [selectedStore, setSelectedStore] = useState(null);
+	const [updatedStore, setUpdatedStore] = useState(selectedStore);
+	const [successMessage, setSuccessMessage] = useState('');
+	// for filters
+	const [filterStoreName, setFilterStoreName] = useState('');
+	const [filterCountry, setFilterCountry] = useState('');
+	const [filterState, setFilterState] = useState('');
+	const [filterAddress, setFilterAddress] = useState('');
+	const [filterPhone, setFilterPhone] = useState('');
+	const [filterDiscountRate, setFilterDiscountRate] = useState('');
+	const [filterPrimeRate, setFilterPrimeRate] = useState('');
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const handleDelete = (storeId) => {
+		const updatedStores = stores.filter((store) => store.id !== storeId);
+		localStorage.setItem('stores', JSON.stringify(updatedStores));
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		if (selectedStore && selectedStore.id === storeId) {
+			setSelectedStore(null);
+			setUpdatedStore(null);
+		}
+		setStores(updatedStores);
+	};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+	const handleUpdateStore = (storeId) => {
+		const storeToUpdate = stores.find((store) => store.id === storeId);
+		setSelectedStore(storeToUpdate);
+		setUpdatedStore({ ...storeToUpdate });
+	};
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+	const handleCancelUpdate = () => {
+		setSelectedStore(null);
+		setUpdatedStore(null);
+	};
+
+	const handleSaveStore = () => {
+		const updatedStores = stores.map((store) => (store.id === updatedStore.id ? updatedStore : store));
+		localStorage.setItem('stores', JSON.stringify(updatedStores));
+		setStores(updatedStores);
+		setSelectedStore(null);
+		setUpdatedStore(null);
+		console.log(stores);
+		setSuccessMessage('Mağaza bilgisi güncellendi!!');
+		setTimeout(() => {
+			setSuccessMessage('');
+		}, 2000);
+	};
+
+	useEffect(() => {
+		localStorage.setItem('stores', JSON.stringify(stores));
+	}, [stores]);
+
+	const filteredStores = stores.filter((store) => {
+		return (
+			store.storeName.toLowerCase().includes(filterStoreName.toLowerCase()) &&
+			store.country.toLowerCase().includes(filterCountry.toLowerCase()) &&
+			store.state.toLowerCase().includes(filterState.toLowerCase()) &&
+			store.address.toLowerCase().includes(filterAddress.toLowerCase()) &&
+			store.phone.toLowerCase().includes(filterPhone.toLowerCase()) &&
+			store.discountRate.toString().includes(filterDiscountRate) &&
+			store.primeRate.toString().includes(filterPrimeRate)
+		);
+	});
+
+	return (
+		<div className='w-full p-20 min-w-full'>
+			{successMessage && (
+				<div className='bg-green-500 text-white text-center p-2 mb-4 rounded'>{successMessage}</div>
+			)}
+			<div className='flex flex-col items-center min-w-full '>
+				<table className='min-w-full text-left text-sm font-light bg-white border border-gray-200 table-fixed overflow-x-auto'>
+					<thead>
+						<tr>
+							<th className='p-3 border-b'>
+								Mağaza İsmi
+								<div className='mt-2 w-[10%]'>
+									<input
+										className='border rounded-md py-1 px-4'
+										type='text'
+										placeholder='Filter by Store Name'
+										value={filterStoreName}
+										onChange={(e) => setFilterStoreName(e.target.value)}
+									/>
+								</div>
+							</th>
+							<th className='p-3 border-b'>
+								Ülke
+								<div className='mt-2 w-[10%]'>
+									<input
+										className='border rounded-md py-1 px-4'
+										type='text'
+										placeholder='Filter by Country'
+										value={filterCountry}
+										onChange={(e) => setFilterCountry(e.target.value)}
+									/>
+								</div>
+							</th>
+							<th className='p-3 border-b'>
+								İl/Şehir
+								<div className='mt-2  w-[10%]'>
+									<input
+										className='border rounded-md py-1 px-4'
+										type='text'
+										placeholder='Filter by State'
+										value={filterState}
+										onChange={(e) => setFilterState(e.target.value)}
+									/>
+								</div>
+							</th>
+							<th className='p-3 border-b'>
+								Adres
+								<div className='mt-2  w-[10%]'>
+									<input
+										className='border rounded-md py-1 px-4'
+										type='text'
+										placeholder='Filter by Address'
+										value={filterAddress}
+										onChange={(e) => setFilterAddress(e.target.value)}
+									/>
+								</div>
+							</th>
+							<th className='p-3 border-b'>
+								Telefon
+								<div className='mt-2 w-[10%]'>
+									<input
+										className='border rounded-md py-1 px-4'
+										type='text'
+										placeholder='Filter by Phone'
+										value={filterPhone}
+										onChange={(e) => setFilterPhone(e.target.value)}
+									/>
+								</div>
+							</th>
+							<th className='p-3 border-b'>
+								İndirim Oranı(%)
+								<div className='mt-2 w-[10%]'>
+									<input
+										className='border rounded-md py-1 px-4'
+										type='text'
+										placeholder='Filter by Discount Rate'
+										value={filterDiscountRate}
+										onChange={(e) => setFilterDiscountRate(e.target.value)}
+									/>
+								</div>
+							</th>
+							<th className='p-3 border-b'>
+								Prim Oranı(%)
+								<div className='mt-2 w-[10%]'>
+									<input
+										className='border rounded-md py-1 px-4'
+										type='text'
+										placeholder='Filter by Prime Rate'
+										value={filterPrimeRate}
+										onChange={(e) => setFilterPrimeRate(e.target.value)}
+									/>
+								</div>
+							</th>
+							<th className='p-3 border-b'>Açıklama</th>
+							<th className='p-3 border-b'>İşlemler</th>
+						</tr>
+					</thead>
+					<tbody>
+						{filteredStores?.map((store, i) => (
+							<tr
+								id={i}
+								key={i}
+								className='border-b transition duration-300 ease-in-out odd:bg-red-50 even:bg-blue-50 hover:bg-red-300'>
+								<td className='p-3 border-b'>{store.storeName}</td>
+								<td className='p-3 border-b'>{store.country}</td>
+								<td className='p-3 border-b'>{store.state}</td>
+								<td className='p-3 border-b'>{store.address}</td>
+								<td className='p-3 border-b'>{store.phone}</td>
+								<td className='p-3 border-b'>{store.discountRate}</td>
+								<td className='p-3 border-b'>{store.primeRate}</td>
+								<td className='p-3 border-b'>{store.description}</td>
+								<td className='p-3 border-b'>
+									<button
+										onClick={() => handleUpdateStore(store.id)}
+										className='bg-blue-500 text-white px-4 py-2 rounded mr-2 transition duration-300 ease-in-out hover:bg-blue-600 hover:shadow-md'>
+										Güncelle
+									</button>
+									<button
+										onClick={() => handleDelete(store.id)}
+										className='bg-red-500 text-white px-4 py-2 rounded transition duration-300 ease-in-out hover:bg-red-600 hover:shadow-md'>
+										Sil
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+
+				{selectedStore && (
+					<div className='mt-4 w-full flex'>
+						<form className='w-full flex flex-wrap'>
+							<label
+								className='w-full my-4 flex justify-between items-center px-3 sm:w-1/2 lg:w-1/3
+              '>
+								<span className='text-gray-700 mr-2'>Mağaza İsmi :</span>
+
+								<input
+									className='form-input mt-1 bg-gray-50 border border-gray-300 rounded-xl focus:ring-blue-500 focus:border-blue-500 py-1 px-3 w-[60%]'
+									type='text'
+									value={updatedStore.storeName}
+									onChange={(e) =>
+										setUpdatedStore({
+											...updatedStore,
+											storeName: e.target.value,
+										})
+									}
+								/>
+							</label>
+							<label className='w-full  my-4 flex justify-between items-center px-3 sm:w-full md:w-1/2 lg:w-1/3 '>
+								<span className='text-gray-700 mr-2'>Ülke :</span>
+
+								<input
+									className='form-input mt-1 bg-gray-50 border border-gray-300 rounded-xl focus:ring-blue-500 focus:border-blue-500 py-1 px-3 w-[60%]'
+									type='text'
+									value={updatedStore.country}
+									onChange={(e) =>
+										setUpdatedStore({
+											...updatedStore,
+											country: e.target.value,
+										})
+									}
+								/>
+							</label>
+							<label className='w-full  my-4 flex justify-between items-center px-3 sm:w-full md:w-1/2 lg:w-1/3'>
+								<span className='text-gray-700 mr-2'>İl/Şehir :</span>
+
+								<input
+									className='form-input mt-1 bg-gray-50 border border-gray-300 rounded-xl focus:ring-blue-500 focus:border-blue-500 py-1 px-3 w-[60%]'
+									type='text'
+									value={updatedStore.state}
+									onChange={(e) =>
+										setUpdatedStore({
+											...updatedStore,
+											state: e.target.value,
+										})
+									}
+								/>
+							</label>
+							<label className='w-full  my-4 flex justify-between items-center px-3 sm:w-full md:w-1/2 lg:w-1/3 '>
+								<span className='text-gray-700 mr-2'>Adres :</span>
+
+								<input
+									className='form-input mt-1 bg-gray-50 border border-gray-300 rounded-xl focus:ring-blue-500 focus:border-blue-500 py-1 px-3 w-[60%]'
+									type='text'
+									value={updatedStore.address}
+									onChange={(e) =>
+										setUpdatedStore({
+											...updatedStore,
+											address: e.target.value,
+										})
+									}
+								/>
+							</label>
+							<label className='w-full my-4 flex justify-between items-center px-3 sm:w-full md:w-1/2 lg:w-1/3'>
+								<span className='text-gray-700 mr-2'>Telefon :</span>
+
+								<input
+									className='form-input mt-1 bg-gray-50 border border-gray-300 rounded-xl focus:ring-blue-500 focus:border-blue-500 py-1 px-3 w-[60%]'
+									type='text'
+									value={updatedStore.phone}
+									onChange={(e) =>
+										setUpdatedStore({
+											...updatedStore,
+											phone: e.target.value,
+										})
+									}
+								/>
+							</label>
+							<label className='w-full my-4 flex justify-between items-center px-3 sm:w-full md:w-1/2 lg:w-1/3'>
+								<span className='text-gray-700 mr-2'>İndirim Oranı :</span>
+
+								<input
+									className='form-input mt-1 bg-gray-50 border border-gray-300 rounded-xl focus:ring-blue-500 focus:border-blue-500 py-1 px-3 w-[60%]'
+									type='text'
+									value={updatedStore.discountRate}
+									onChange={(e) =>
+										setUpdatedStore({
+											...updatedStore,
+											discountRate: e.target.value,
+										})
+									}
+								/>
+							</label>
+							<label className='w-full  my-4 flex justify-between items-center px-3 sm:w-full md:w-1/2 lg:w-1/3 '>
+								<span className='text-gray-700 mr-2'>Prime Oranı : </span>
+
+								<input
+									className='form-input mt-1 bg-gray-50 border border-gray-300 rounded-xl focus:ring-blue-500 focus:border-blue-500 py-1 px-3 w-[60%]'
+									type='text'
+									value={updatedStore.primeRate}
+									onChange={(e) =>
+										setUpdatedStore({
+											...updatedStore,
+											primeRate: e.target.value,
+										})
+									}
+								/>
+							</label>
+							<label className='w-full  my-4 flex justify-between items-center px-3 sm:w-full md:w-1/2 lg:w-1/3'>
+								<span className='text-gray-700 mr-2'>Açıklama :</span>
+
+								<textarea
+									cols='40'
+									rows='5'
+									className='form-input mt-1 bg-gray-50 border border-gray-300 rounded-xl focus:ring-blue-500 focus:border-blue-500 py-1 px-3 w-[60%]'
+									type='text'
+									value={updatedStore.description}
+									onChange={(e) =>
+										setUpdatedStore({
+											...updatedStore,
+											description: e.target.value,
+										})
+									}
+								/>
+							</label>
+							<div className='w-full my-4 flex justify-center items-center gap-3 px-3 sm:w-full md:w-1/2 lg:w-1/3'>
+								<button
+									type='button'
+									onClick={handleSaveStore}
+									className='bg-blue-500 text-white px-4 max-h-12 py-2 rounded-md transition duration-300 ease-in-out hover:bg-blue-600 hover:shadow-md'>
+									Kaydet
+								</button>
+
+								<button
+									type='button'
+									onClick={handleCancelUpdate}
+									className='bg-gray-500 text-white px-4 max-h-12 py-2 rounded-md transition duration-300 ease-in-out hover:bg-gray-600 hover:shadow-md'>
+									İptal
+								</button>
+							</div>
+						</form>
+					</div>
+				)}
+
+				<p className='mt-4'>
+					<Link href='/add-store'>
+						<button className='bg-blue-500 text-white px-4 py-2 rounded-md transition duration-300 ease-in-out hover:bg-blue-600 hover:shadow-md'>
+							Mağaza Ekle
+						</button>
+					</Link>
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
